cb05c0591a78365231c44cf44c7a4b251c002312
Dandara Aryadne <dandaraaryadne@hotmail.com>
2022-01-23 18:58:57 -0300

    primeira parte do desafio

---
+++ script.js
@@ -0,0 +1,42 @@
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+let age = 30
+if(age >= 18){
+    console.log("Passou rapá! Acessa ai!")
+}else{
+    console.log("Jovem, volte 3 casas e toma vergonha!")
+}
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a variável "humano" seja true
+let humano = "true"
+if(age >= 18 && humano == "true"){
+    console.log("Chega mais, tu é um humano de maior")
+}else{
+    console.log("Humano de maior você não é")
+}
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+let aniversário ="Novembro"
+if(aniversário == "Janeiro" || aniversário == "Dezembro"){
+    console.log("Chegou seu mês! Parabéns!")
+}else{
+    console.log("Seu aniversário ainda não chegou!")
+}
+
+//Sem enunciado// 
+
+let age =30
+if(age >=18){
+    console.log ("Passou rapá! Acessa ai!")
+}else{
+    console.log("Jovem, volte 3 casas e toma vergonha!")
+}
+let humano = "true"
+if(age >= 18 && humano == "true"){
+    console.log("Chega mais, tu é um humano de maior")
+}else{
+    console.log("Humano de maior você não é")
+}
+let aniversário ="Novembro"
+if(aniversário == "Janeiro" || aniversário == "Dezembro"){
+    console.log("Chegou seu mês! Parabéns!")
+}else{
+    console.log("Seu aniversário ainda não chegou!")
+}
